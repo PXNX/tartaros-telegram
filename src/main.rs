c13bc8c6fea1229a46474306784f5eb7040d2cee
nyx69 <48032272+PXNX@users.noreply.github.com>
2022-06-22 00:20:13 +0200

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use axum::{routing::get, Router};
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(|| async { "Hello, Wordghhhhld!" }));
+
+    let port = std::env::var("PORT")
+        .ok()
+        .and_then(|s| s.parse().ok())
+        .unwrap_or(3000);
+
+    let address = SocketAddr::from(([0, 0, 0, 0], port));
+
+    axum::Server::bind(&address)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
