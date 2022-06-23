3cbed6f820f0f918b6eb26c001ac869d8435e681
nyx69 <48032272+PXNX@users.noreply.github.com>
2022-06-23 23:28:48 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,14 @@ pub mod schema;
 
 use rocket::serde::{Deserialize, Serialize};
 
+#[macro_use]
+extern crate diesel;
+use rocket::serde::{Deserialize, Serialize};
+use rocket_sync_db_pools::database;
+
+#[database("db")]
+pub struct PgConnection(diesel::PgConnection);
+
 #[derive(Serialize, Deserialize, Debug)]
 #[serde(crate = "rocket::serde")]
 pub struct ApiError {
