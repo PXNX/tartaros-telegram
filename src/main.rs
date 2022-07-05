e85adfbace8d57fb80dfc2913e764a96fcd35ced
nyx69 <48032272+PXNX@users.noreply.github.com>
2022-07-05 10:28:14 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -192,7 +192,7 @@ async fn report_user(
                     user_id: report.user_id,
                     user_msg: String::from(&report.user_msg),
                 })
-                .get_result::<Report>(c)
+                .get_result::<Report>(c).await
         })
         .await;
 
